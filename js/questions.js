6f33e0eda23eb548762974342197844777a00b09
NihedxTn <102193839+Nihed-Abd@users.noreply.github.com>
2023-10-25 00:15:04 +0100

    second commit

---
+++ js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " What is the definition of mental health?",
+    answer: "A state of well-being in which an individual realizes their abilities, can cope with life's stresses, can work productively, and contribute to their community.",
+    options: [
+      "The absence of all negative emotions.",
+      "A state of well-being in which an individual realizes their abilities, can cope with life's stresses, can work productively, and contribute to their community.",
+      " The ability to always be happy and carefree.",
+      "The absence of mental illness."
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a common symptom of stress?",
+    answer: "Improved concentration and focus",
+    options: [
+      " Increased heart rate and blood pressure",
+      "Improved concentration and focus",
+      "Muscle tension and headaches",
+      "Sleep disturbances"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the term for a prolonged feeling of sadness and hopelessness, often accompanied by a loss of interest in activities and a lack of energy?",
+    answer: "Depression",
+    options: [
+      "Anxiety",
+      "Bipolar disorder",
+      "Schizophrenia",
+      "Depression"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of the following activities can help improve mental well-being?",
+    answer: "Regular exercise",
+    options: [
+      "Isolating yourself from friends and family",
+      "Regular exercise",
+      "Dwelling on negative thoughts",
+      "Avoiding seeking professional help"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What is the primary goal of therapy or counseling in mental health care?",
+    answer: "To provide a safe and supportive space for individuals to explore their thoughts and feelings, gain insight, and develop coping strategies",
+    options: [
+      "To solve all of an individual's problems",
+      "To prescribe medication",
+      "To provide a safe and supportive space for individuals to explore their thoughts and feelings, gain insight, and develop coping strategies",
+      "To convince individuals that their problems are not significant"
+    ]
+  },
+  // {
+  //   numb: 6,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+  // {
+  //   numb: 7,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+  // {
+  //   numb: 8,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+  // {
+  //   numb: 9,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+  // {
+  //   numb: 10,
+  //   question: "What does HTML stand for?",
+  //   answer: "Hyper Text Markup Language",
+  //   options: [
+  //     "Hyper Text Preprocessor",
+  //     "Hyper Text Markup Language",
+  //     "Hyper Text Multiple Language",
+  //     "Hyper Tool Multi Language"
+  //   ]
+  // },
+
+];
